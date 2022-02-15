f16d41a2fbae92aedfc5ad836660cafcebc4780c
Doniyorbek <donyorbek9043@gmail.com>
2022-02-15 10:56:15 +0300

    commit

---
+++ resources/views/admin/klaster/index.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.master')
+@section('content')
+
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-9"><h1 class="card-title">сотрудники</h1></div>
+                    <div class="col-md-1">
+                        <a class="btn btn-primary" href="{{route('admin.staff.create')}}">
+                            <span class="btn-label">
+                                <i class="fa fa-plus"></i>
+                            </span>
+                            Добавить сотрудники
+                        </a>
+                    </div>
+                </div>
+                <hr>
+                <div class="card-body">
+                    <table width="90%" class="table  table-bordered">
+                        <thead>
+                        <tr>
+
+                            <th  scope="col">ismi familyasi</th>
+                            
+                            <th  scope="col">Действие</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($klasteres as $klaster)
+                                <tr>
+
+                                    <td >{{$klaster->name}}</td>
+                                   
+                                    <td>
+                                        <form action="{{ route('admin.klaster.destroy',$klaster ->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <a class="btn btn-warning btn-sm" href="{{ route('admin.klaster.edit',$klaster->id) }}">
+                                    <span class="btn-label">
+                                        <i class="fa fa-pen"></i>
+                                    </span>
+
+                                            </a>
+
+                                            <button type="submit" class="btn btn-danger btn-sm"><span class="btn-label">
+                                        <i class="fa fa-trash"></i>
+                                    </span></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
